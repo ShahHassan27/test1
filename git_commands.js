beb998588c8a61ed649c37a15ef891d3b2aa28a3
ShahHassan27 <s4h7hassan@gmail.com>
2020-09-24 14:47:22 +0900

    added description of commit

--- git_commands.js
+++ git_commands.js
@@ -2,7 +2,16 @@
 
     git config -- global user.email s4h7hassan@gmail.com
 
-    
+    git status  [Modified]
+
+    git add git_commands.js  [Staging]
+
+    (use "git rm --cached <file>..." to unstage)
+
+    git add . [add all]
+
+    git commit -m "added new commands for github"   [commit is a snap shot like a check point useful to save different versions]
+
 
 
  */
